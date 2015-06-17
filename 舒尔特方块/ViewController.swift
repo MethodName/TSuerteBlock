c1ae1df74cb4899758457d310812e704c54318cd
methodname <methodname@qq.com>
2015-06-17 15:19:42 +0800

    Initial Commit

---
+++ 舒尔特方块/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  舒尔特方块
+//
+//  Created by tangmingming on 15-6-17.
+//  Copyright (c) 2015年 tangmingming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
